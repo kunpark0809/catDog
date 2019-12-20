93089702d97b21eba0db4996909d58961addbd30
sist <sist@DESKTOP-VJFC96L>
2019-12-20 17:00:18 +0900

    신승연 - 카드결제까지 완료

--- catDog/src/main/java/com/catDog/pay/PayController.java
+++ catDog/src/main/java/com/catDog/pay/PayController.java
@@ -1,11 +1,15 @@
 package com.catDog.pay;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.catDog.customer.SessionInfo;
 
 @Controller("pay.payController")
 public class PayController {
@@ -16,21 +20,33 @@ public class PayController {
 	public String payForm(
 			@RequestParam int productNum,
 			@RequestParam int quantity,
-			Model model
+			Model model,
+			HttpSession session
 			) throws Exception{
 		Pay product = service.readProudct(productNum);
 		product.setProductCount(quantity);
 		product.setProductSum(product.getPrice()*quantity);
 		product.setPoint((int)(product.getProductSum() * 0.01));
+		
+		SessionInfo info = (SessionInfo)session.getAttribute("member");
+		Pay customer = null;
+		if(info != null) {
+			customer = service.readCustomer(info.getMemberIdx());
+		}
+		model.addAttribute("customer",customer);
 		model.addAttribute("product",product);
 		return ".pay.pay";
 	}
 	
 	@RequestMapping(value="/pay/pay", method=RequestMethod.POST)
 	public String paySubmit(
-			@RequestParam String productNum 
+			@RequestParam String productNum, 
+			Pay pay
 			) throws Exception{
-		return ".pay.pay";
+
+			
+		service.insertRequest(pay);
+		return ".pay.complete";
 	}
 	
 	@RequestMapping(value="/pay/complete")
--- catDog/src/main/java/com/catDog/pay/PayService.java
+++ catDog/src/main/java/com/catDog/pay/PayService.java
@@ -2,4 +2,7 @@ package com.catDog.pay;
 
 public interface PayService {
 	public Pay readProudct(int productNum);
+	public Pay readCustomer(long num);
+	public void insertCustomer(Pay pay) throws Exception;
+	public void insertRequest(Pay pay) throws Exception;
 }
