ac63248145e233b8755379f2ce3c10a65b3e857a
sist <sist@DESKTOP-R9U8PVB>
2020-01-16 18:19:03 +0900

    서현(페이징 후버 색 조정)

--- catDog/src/main/java/com/catDog/common/MyUtil.java
+++ catDog/src/main/java/com/catDog/common/MyUtil.java
@@ -51,8 +51,8 @@ public class MyUtil {
 
 		sb.append("<style type='text/css'>");
 		sb.append("#paginate {clear:both;font:12px \"맑은 고딕\",NanumGothic,돋움,Dotum,AppleGothic;padding:15px 0px 0px 0px;text-align:center;height:28px;white-space:nowrap;}");
-		sb.append("#paginate a {border:1px solid #ccc;height:28px;color:#000000;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;outline:none; select-dummy: expression(this.hideFocus=true);}");
-		sb.append("#paginate a:hover, a:active {border:1px solid #ccc;color:#6771ff;vertical-align:middle; line-height:normal;}");
+		sb.append("#paginate a {border:1px solid #ccc;height:28px;color:black;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;outline:none; select-dummy: expression(this.hideFocus=true);}");
+		sb.append("#paginate a:hover, a:active {border:1px solid #ccc;color:black;vertical-align:middle; line-height:normal;}");
 		sb.append("#paginate .curBox {border:1px solid #f3a34e; background: #f3a34e; color:white; font-weight:bold;height:28px;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
 		sb.append("#paginate .numBox {border:1px solid #f3a34e; color:#f3a34e;height:28px;font-weight:bold;text-decoration:none;padding:4px 7px 4px 7px;margin-left:3px;line-height:normal;vertical-align:middle;}");
 		sb.append("</style>");
